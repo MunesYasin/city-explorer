f084a4461ecf704bd55b60f04f079af08364ba79
MunesYasin <m2nesyasin@gmail.com>
2021-09-19 19:20:01 +0300

    solve lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react'
+import { Form } from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+        thePlace :'',
+        locationResult : {},
+        show : false
+    }
+  }
+
+  getDirection = async (e)=>{
+    e.preventDefault()
+  await this.setState({
+  thePlace : e.target.place.value
+})
+  
+
+   let urlRequest = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER}&q=${this.state.thePlace}&format=json`
+
+console.log(urlRequest)
+  let dirResult = await axios.get(urlRequest)
+  console.log(dirResult)
+
+
+this.setState({
+  locationResult : dirResult [0],
+  show : true
+})
+
 }
 
-export default App;
+
+
+  render(){
+    return(
+      <div>
+        <h3>City Explorer App</h3>
+        <Form onSubmit={this.getDirection}>
+        <input type='text' name='place'/>
+        <input type='submit' value='get direction'/>
+
+        </Form>
+        {this.state.show && 
+        <>
+        <p>City Name : {this.state.thePlace}</p>
+        <p>Latitude : {this.state.locationResult.lat}</p>
+        <p>longititude : {this.state.locationResult.lon}</p>
+
+        <img  src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER}&center=${this.state.locationResult.lat},this.state.locationResult.lon&zoom=10`}/>
+        </>
+        }
+      </div>
+    )
+  }
+}
+export default App
